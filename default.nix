1646bf9103b83192f2f5b97ffd61c114db0656ed
justinwoo <moomoowoo@gmail.com>
2024-03-21 01:23:17 +0200

    update tie to 20240321

--- default.nix
+++ default.nix
@@ -15,31 +15,24 @@ in
 pkgs.stdenv.mkDerivation rec {
   pname = "tie";
 
-  version = "20240305";
+  version = "20240321";
 
   buildInputs = [ pkgs.gmp  ];
 
   libPath = pkgs.lib.makeLibraryPath buildInputs;
-
-  tie-repo = pkgs.fetchFromGitHub {
-    owner= "scarf-sh";
-    repo= "tie";
-    rev= "efcc04057d1c362e4eb89fe7e713f84a8a229cc6";
-    hash= "sha256-9NJ0jFcFUV+4YfwsGzm16ETT+sHG1OkgqgNyD0mpOiY=";
-  };
 
   src =
     if pkgs.stdenv.isDarwin
     then
       pkgs.fetchzip
         {
-          url = "justin.gateway.scarf.sh/easy-tie-nix/macos-latest/20240305.tar.gz";
-          sha256 = "fxh+9n3MoQMXumbnChNORFuK2XBvzBlPX9u4HQra/ro=";
+          url = "justin.gateway.scarf.sh/easy-tie-nix/macos-latest/20240321.tar.gz";
+          sha256 = "zlGV3OZ1ZcLraJEcWse/sri984mmnEW6bmrhk0C2QEY=";
         }
     else
       pkgs.fetchzip {
-        url = "justin.gateway.scarf.sh/easy-tie-nix/ubuntu-latest/20240305.tar.gz";
-        sha256 = "ExGQq7aDt4pDLcw+eCXbDVBe64jcrXLUmfdo+rHcCkA=";
+        url = "justin.gateway.scarf.sh/easy-tie-nix/ubuntu-latest/20240321.tar.gz";
+        sha256 = "SaTC7ItBt0RY7bPZKBsEp/uFYtzk3Xi3EAc2Pn2FoRo=";
       };
 
   dontStrip = true;
@@ -53,11 +46,5 @@ pkgs.stdenv.mkDerivation rec {
 
     mkdir -p $out/etc/bash_completion.d/
     $TIE --bash-completion-script $TIE > $out/etc/bash_completion.d/tie-completion.bash
-
-    mkdir -p $out/share
-    cp ${tie-repo}/*template* $out/share
-
-    mkdir -p $out/share/test
-    cp ${tie-repo}/test/golden -r $out/share/test
   '';
 }
