3d3e46f18777469a23b9850931501d68b9747a67
justinwoo <moomoowoo@gmail.com>
2024-03-20 23:20:14 +0200

    update example

--- example.nix
+++ example.nix
@@ -6,7 +6,7 @@ import
     {
       owner = "justinwoo";
       repo = "easy-tie-nix";
-      rev = "d227a301912e524d3f04130b4584a523eed67e08";
-      sha256 = "1dj8y8c387z7wrkfvrjhvak5xiwxxjz1h0wl6vbzfppnhlz4wd1p";
+      rev = "e92ef16c2ac9964bdb1e27e7eb4b2393ed8a61e0";
+      sha256 = "30U5vdHmxVqn0TMxL0TGXKAUkcvHEqH08K0k6checHQ=";
     })
 { }
